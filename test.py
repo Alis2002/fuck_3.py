0cc00aa0557cba321ab096dc67e69449a4f3bcb7
Alis2002 <ak@nsdigital.ru>
2022-01-22 15:32:50 +0300

    last sta

--- test.py
+++ test.py
@@ -4,26 +4,45 @@ import os.path
 import sys
 from fuck_3 import getTextFile
 
-local_link = r"C:/prog/quest/text.xt"
-print(getTextFile(local_link))
+
 pdf = FPDF(orientation='l', format='A4')
 pdf.set_font('Times')
 
-fos_ware = r"C:\prog\PDF\wave.PNG"
+fos_ware = r"C:\prog\pythonPr\wave.PNG"
 arr_q = [f for f in glob.glob("files*.jpeg")]
 text_sorted = sorted([f for f in glob.glob("files*jpeg.txt")])
-ware = r"C:/prog/quest/"
-for x in text_sorted:
-    a = ware + x
-print(a)
-
-
-
+ware = r"C:/prog/pythonPr/"
 sort = sorted(arr_q)
+for x in text_sorted:
+    local_link = ware + x
+    get_link = (getTextFile(local_link))
+    print(local_link)
+cent_q = 1
 
+
+
+
+def create_counter():
+    i = 0
+
+    def func():
+        nonlocal i
+        i += 1
+        return i
+
+    return func
+
+
+counter = create_counter()
 for image_file in sort:
     pdf.add_page()
-    pdf.image(fos_ware, w=210, x=0, y=0)
-    pdf.image(image_file, w=190, x=35, y=20)
+    pdf.image(image_file, w=210, x=35, y=20)
+    pdf.set_fill_color(173, 255, 47)
+    pdf.cell(w=80, h=90, txt='hgvhg', border='T'+'L', ln=2, align='C', fill=True, link='')
+
+
+    # pdf.image(image_file, w=210, x=35, y=20)
+()
+
 
 pdf.output("image.pdf")
